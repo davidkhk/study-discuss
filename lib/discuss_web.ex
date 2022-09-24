bc7b2c6a0de0eee0b8b2fc8df57ef1afdfa9b944
David K <davidkhk@gmail.com>
2022-09-24 18:45:44 -0300

    fix: add model to discuss_web

--- lib/discuss_web.ex
+++ lib/discuss_web.ex
@@ -42,6 +42,16 @@ defmodule DiscussWeb do
     end
   end
 
+  def model do
+    quote do
+      use Ecto.Schema
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query
+    end
+  end
+
   def live_view do
     quote do
       use Phoenix.LiveView,
