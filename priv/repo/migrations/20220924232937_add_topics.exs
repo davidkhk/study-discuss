8aac8355e3178efbaca7870e6733eede8092d28a
David K <davidkhk@gmail.com>
2022-09-24 20:33:36 -0300

    fix: replace table on db

--- priv/repo/migrations/20220924232937_add_topics.exs
+++ priv/repo/migrations/20220924232937_add_topics.exs
@@ -2,8 +2,8 @@ defmodule Discuss.Repo.Migrations.AddTopics do
   use Ecto.Migration
 
   def change do
-    create table(:topic) do
-      add :title, :string
-    end
+create table(:topics) do
+  add :title, :string
+end
   end
 end
