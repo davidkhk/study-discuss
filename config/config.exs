e26ed0113d2a3e77a23e2a8a627aa9848d8835f6
David K <davidkhk@gmail.com>
2022-09-26 14:51:32 -0300

    feat: insert hidden api keys

--- config/config.exs
+++ config/config.exs
@@ -50,6 +50,7 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+import_config "dev.secret.exs"
 
 config :ueberauth, Ueberauth,
   providers: [
