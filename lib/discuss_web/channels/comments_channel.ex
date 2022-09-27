a6d945078afdce6b1d112df82ce84a8cb278d2af
David K <davidkhk@gmail.com>
2022-09-27 17:21:27 -0300

    feat: create comments channel

---
+++ lib/discuss_web/channels/comments_channel.ex
@@ -0,0 +1,29 @@
+defmodule DiscussWeb.CommentsChannel do
+  use DiscussWeb, :channel
+
+  @impl true
+  def join(name, _params, socket) do
+    IO.puts("+++++++++")
+    IO.puts(name)
+    {:ok, %{hey: "there"}, socket}
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  @impl true
+  def handle_in() do
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (comments:lobby).
+  # @impl true
+  # def handle_in("shout", payload, socket) do
+  #   broadcast(socket, "shout", payload)
+  #   {:noreply, socket}
+  # end
+
+  # Add authorization logic here as required.
+  # defp authorized?(_payload) do
+  #   true
+  # end
+end
