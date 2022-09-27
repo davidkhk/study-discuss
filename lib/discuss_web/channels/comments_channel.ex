6ebf9d6ecd1d8d1375fc3787c9cfc86155428e9d
David K <davidkhk@gmail.com>
2022-09-27 18:02:55 -0300

    feat: start working on comments list

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -1,29 +1,18 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
+  alias DiscussWeb.Topic
+  alias Discuss.Repo
 
   @impl true
-  def join(name, _params, socket) do
-    IO.puts("+++++++++")
-    IO.puts(name)
-    {:ok, %{hey: "there"}, socket}
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, socket}
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
   @impl true
-  def handle_in() do
+  def handle_in(name, message, socket) do
+    {:reply, :ok, socket}
   end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (comments:lobby).
-  # @impl true
-  # def handle_in("shout", payload, socket) do
-  #   broadcast(socket, "shout", payload)
-  #   {:noreply, socket}
-  # end
-
-  # Add authorization logic here as required.
-  # defp authorized?(_payload) do
-  #   true
-  # end
 end
